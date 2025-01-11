799484d64642de414a6fab4ba5bf403dadc74a1b
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 10:42:36 +0100

    chore: string  methods

--- string-methods.py
+++ string-methods.py
@@ -4,6 +4,8 @@ print(name)
 lower_name = "Zeeekkeee".lower() # lower() method converts the string to lower
 print(lower_name)
 
-
 capitalize = "zeeekkeee".capitalize() # capitalize() method converts the string to capitalize format
 print(capitalize)
+
+countString = "zeeekkeee".count('e') # coun the passed substring length of the string
+print(countString)
