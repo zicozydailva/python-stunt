e5c0bb4451663ae977197016d0f79c51a86de806
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 11:27:36 +0100

    chore: for-loop&& git push

---
+++ loops.py
@@ -0,0 +1,3 @@
+# for Loops
+for i in range(10):
+    print(i)
--- tuple.py
+++ tuple.py
@@ -1,3 +1,3 @@
-# tuple - immutable list: cannot be changed
+# tuple - immutable list: cannot be changed... limited methods... cannot append an all
 x = (0,0,2,2)
 print(x[0])
