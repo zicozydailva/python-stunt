59939ea54183839e1b230ff4ad937de72dc8f9dc
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 11:42:41 +0100

    chore: while loop

---
+++ slice-operator.py
@@ -0,0 +1,11 @@
+x = [0,1,2,3,4,5,6]
+y = ['hi', 'hello', 'goodbye', 'cya', 'sure']
+s = 'hello'
+
+# how it works: [start:stop:step] --- just like range for loops
+sliced = x[0:4:2] # [0, 2]
+print(sliced)
+
+# To reverse a list
+reverseList = x[::-1]
+print(reverseList)
