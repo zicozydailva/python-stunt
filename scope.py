06ad6fdf0ce50482b879c58f90f6ee40811910e6
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 15:43:24 +0100

    chore: scope

--- scope.py
+++ scope.py
@@ -1,9 +1,11 @@
 x = "tim"
 
 def func(name):
-    x = name
+    # x = name # this won't change the value of x
+    
+    global x # this will change the value of x - not ideal to use
 
 
-print(x)
-func("joe")
-print(x)
+print(x) # tim
+func("joe") 
+print(x) # tim
