8173b9caf16d3ed688262839409f92dc64a9771e
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 15:01:26 +0100

    chore: comprehensions

--- comprehensions.py
+++ comprehensions.py
@@ -1,4 +1,8 @@
 # comprehensions - one line initialisation of a list, tuple etc
 
 x = [x for x in range(5)]
-print(x)
+print(x) # [0, 1, 2, 3, 4]
+
+
+y = [y + 5 for y in range(5)]
+print(y) # [5, 6, 7, 8, 9]
