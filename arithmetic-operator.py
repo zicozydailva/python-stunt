7220acd1650fa63cf9664c76233ac0e9fdfa7269
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 10:20:10 +0100

    chore: arithmetic operands

--- arithmetic-operator.py
+++ arithmetic-operator.py
@@ -1,4 +1,37 @@
 x = 9
-y = 3.5
+y = 3
+
+# ADDITION
 result = x + y
-print(result)
+print("RESULT ", result)
+
+# SUBTRACTION
+sub = x - y
+print("SUB ", sub)
+
+# MULTIPLICATION
+mul = x * y
+print("MUL ", mul)
+
+# DIVISION  - returns float (no precision)
+div = int(x / y)
+print("DIV ", div)
+
+# EXPONENTIATION
+expo = x ** 2
+print("EXPO ", expo)
+
+expoDiv = x // y
+print("EXPO ", expo)
+
+
+# MODULUS
+mod = x % y
+print("MOD ", mod)
+
+
+#  USER INPUT
+first_num = int(input("Enter a number: "))
+second_num = int(input("Enter another number: "))
+result = first_num + second_num
+print("The result is ", result)
