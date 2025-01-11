d818d8fb6faabc388f3d7bfcac8a6287feddf6dc
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 11:31:10 +0100

    chore: for loop

--- loops.py
+++ loops.py
@@ -1,3 +1,10 @@
 # for Loops
+
+# range takes start, stop, step paramters, passing just one defaults to just "stop", "step" states how our iteration should flow and start is actually the starting point, NOTE: stop value isn't included
+
 for i in range(10):
+    print(i)
+    
+    # start at 1, stop at 9, increment by 2 on every iteration
+for i in range(1,10,2):
     print(i)
