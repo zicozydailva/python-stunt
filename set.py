7ebf69e74b096639495f1655a2d1d1ad4cdec9f8
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 14:51:09 +0100

    chore: while loop

--- set.py
+++ set.py
@@ -1,8 +1,17 @@
 # SET - unique values without duplicates
+# using a set is very beneficial and faster for looking things up
 
 y = {} # typeof dictionary
 x = set() # typeo set: empty set
 
 setWithValues = {4, 32, 3, 3} # a set with values in it
-setWithValues.remove(4)
-print(setWithValues)
+setValues = {4, 32, 3, 2, 8, 3} 
+
+# check if element exist in set
+print(33 in setWithValues) # False
+print(32 in setWithValues) # True
+
+# Validate union of a set
+print(setWithValues.union(setValues))
+print(setWithValues.difference(setValues))
+print(setWithValues.intersection(setValues))
