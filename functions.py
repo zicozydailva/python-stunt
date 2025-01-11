17bd37c34d0cd5a5131782d6282e5015d59b094f
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 15:17:44 +0100

    chore: functions

--- functions.py
+++ functions.py
@@ -1,7 +1,8 @@
-def func():
-    print("Hello World")
-    def func():
-        print("Hello World 2")
-    func()
+def func(x, y):
+    print("Hello World", x, y)
+    return x * y, x/y # returning multiplication and divison value
     
-func()
+print(func(10, 20)) # compacted result
+
+r1, r2 = func(10, 20)
+print(r1, r2) # seperated result
