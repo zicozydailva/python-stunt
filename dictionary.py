f6976a473642037095f61d411dcb933f75355ca8
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 14:56:19 +0100

    chore: del and all print

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,13 @@
 
 x = {"key": 4, "key2": 5, "key3": 6}
 print(x["key2"])
+
+# To check if key exist
+print('key' in x)
+
+# Get all values from dictionary as list
+print(list(x.values()))
+
+# delete key
+del x['key']
+print(list(x.values()))
