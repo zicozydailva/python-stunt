52995c18a0f7185b8f897bc57513962009564d3b
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 15:40:23 +0100

    chore: args and kwargs sample

--- unpack-operator.py
+++ unpack-operator.py
@@ -1,6 +1,17 @@
 def func(*agrs, **kwargs):
-    pass
+    print(agrs, kwargs)
+    
+    
+func(1,2,3,4,5, x=1, y=2, z=3)
 
-x = [1,22,2244,2233]
-print(*x)
+
+
+# x = [1,22,2244,2233]
+# pairs = [(1,2), (3,4), (5,6)]
+
+# for pair in pairs:
+#     func(*pair) # tuple or list example
+#     func(**{'x': 1, 'y': 2}) # dictionary example
+    
+# print(*x)
    
