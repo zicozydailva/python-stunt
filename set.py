9ce2c89c0230500f7f9473511037d37b89995626
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 11:47:17 +0100

    chore: while loop

---
+++ set.py
@@ -0,0 +1,8 @@
+# SET - unique values without duplicates
+
+y = {} # typeof dictionary
+x = set() # typeo set: empty set
+
+setWithValues = {4, 32, 3, 3} # a set with values in it
+setWithValues.remove(4)
+print(setWithValues)
