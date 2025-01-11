3454b4a18efd020499170f931208764bc9022e4a
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 11:14:37 +0100

    chore: and or operand

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,5 @@
 x = 10
+y = 30
 
 if x > 20 :
     print("X is greater than 20")
@@ -16,3 +17,7 @@ if x > 20 or x < 20:
 # OR NOT OPERAND
 if not x > 20:
     print("X is not greater than 20")
+    
+# AND OPERAND
+if x > 5 and not x > 15:
+    print("X is between the range of 5-15")
