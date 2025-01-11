6a8643bb2314273428f5649c407b90cc82f77c3c
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-01-11 10:41:18 +0100

    chore: string  methods

--- string-methods.py
+++ string-methods.py
@@ -1,3 +1,9 @@
 name = "zeeekkeee".upper() # upper() method converts the string to uppercase
 print(name)
 
+lower_name = "Zeeekkeee".lower() # lower() method converts the string to lower
+print(lower_name)
+
+
+capitalize = "zeeekkeee".capitalize() # capitalize() method converts the string to capitalize format
+print(capitalize)
